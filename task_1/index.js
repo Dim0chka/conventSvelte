fa415f2633712700b49acb5c3d2efba4f00c64ab
Dim0chka <Bugrick2004@yandex.ru>
2024-04-23 01:55:44 +0300

    Написание кода для task_1

--- task_1/index.js
+++ task_1/index.js
@@ -1,8 +1,34 @@
 import {encoded, translations} from './data.js'
 
-console.log("Let's rock")
-console.log(encoded, translations)
+
+function result(encoded, translations) {
+    const exep = [
+        "groupId",
+        "service",
+        "formatSize",
+        "ca"
+    ]
+    
+    encoded.forEach(obj => {
+        Object.entries(obj).forEach(([key, value]) => {
+            if (!exep.includes(key)) {
+                translate(value) ? obj[key] = translate(value) : delete obj[key]
+            } 
+        })
+    })
+    
+    
+    function translate(el) {
+        if (el === "0") {
+            return ""
+        } else {
+            return translations[el] || false
+        }
+    }
+
+    return encoded
+}
 
 
+console.log(result(encoded, translations))
 
-// console.log(decoded)
